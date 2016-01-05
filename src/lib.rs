d166cfce30253910e3f74b8fae156d320ddbc1fe
James Carson <jms.crsn@gmail.com>
2016-01-04 23:43:04 -0500

    successfully can convert an array of numbers to json

--- src/lib.rs
+++ src/lib.rs
@@ -16,14 +16,27 @@ enum Value {
 type Array  = Vec<Value>;
 type Object = HashMap<String,Value>;
 
+impl ToJson for Value {
+    fn to_json(&self) -> String {
+        match *self {
+            Value::Number(x) => format!("{}",x),
+            _ => format!("{}",666),
+        }
+    }
+}
+
 impl ToJson for Array {
     fn to_json(&self) -> String {
-        return format!("{}",5);
+        let mut result: String = String::new();
+        result.push_str("[");
+        result.push_str( &self.iter().map(|value| value.to_json()).collect::<Vec<String>>().join(",") );
+        result.push_str("]");
+        result
     }
 }
 
 #[test]
 fn to_json_sanity_check() {
     let some_array: Array = vec![Value::Number(1f64),Value::Number(2f64)];
-    assert_eq!(some_array.to_json(),"5".to_string());
+    assert_eq!(some_array.to_json(),"[1,2]".to_string());
 }
